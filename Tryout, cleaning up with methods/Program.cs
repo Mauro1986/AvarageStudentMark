ace7adff37bf992b71fa0880969ec55e77ffb1cc
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2020-10-26 08:22:12 +0100

    oefenen

--- Tryout, cleaning up with methods/Program.cs
+++ Tryout, cleaning up with methods/Program.cs
@@ -10,6 +10,22 @@ namespace Tryout__cleaning_up_with_methods
     {
         static void Main(string[] args)
         {
+
+
+            int[] ArrayNr = { 10, 5, 60, 8, 2 };
+
+            int biggestNr = 60;
+            //int index = 0;
+
+            for (int i = 0; i < ArrayNr.Length; i++)
+            {
+                if (ArrayNr[i] > biggestNr)
+                {
+                    biggestNr = ArrayNr[i];
+                    //index = Array.IndexOf(ArrayNr, ArrayNr[i]);
+                }
+            }
+            Console.WriteLine($"The biggest Nr = {biggestNr}");
         }
     }
 }
